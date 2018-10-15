d01131a58c6305210c33d25d455eb87006de9e5c
Simon Parker <engineeringsimon@yahoo.com.au>
2018-10-16 10:59:54 +1100

    Auto test script start

---
+++ scripts/run_test.ps1
@@ -0,0 +1,41 @@
+.\scripts\run_match.ps1 .\algos\Bumi .\algos\starter-algo > Bumi_starter-algo_00.txt
+.\scripts\run_match.ps1 .\algos\Bumi .\algos\starter-algo > Bumi_starter-algo_01.txt
+.\scripts\run_match.ps1 .\algos\Bumi .\algos\starter-algo > Bumi_starter-algo_02.txt
+.\scripts\run_match.ps1 .\algos\Bumi .\algos\BadgerMole   > Bumi_BadgerMole_00.txt
+.\scripts\run_match.ps1 .\algos\Bumi .\algos\BadgerMole   > Bumi_BadgerMole_01.txt
+.\scripts\run_match.ps1 .\algos\Bumi .\algos\BadgerMole   > Bumi_BadgerMole_02.txt
+.\scripts\run_match.ps1 .\algos\Bumi .\algos\Dragon       > Bumi_Dragon_00.txt
+.\scripts\run_match.ps1 .\algos\Bumi .\algos\Dragon       > Bumi_Dragon_01.txt
+.\scripts\run_match.ps1 .\algos\Bumi .\algos\Dragon       > Bumi_Dragon_02.txt
+.\scripts\run_match.ps1 .\algos\Bumi .\algos\SkyBison     > Bumi_SkyBison_00.txt
+.\scripts\run_match.ps1 .\algos\Bumi .\algos\SkyBison     > Bumi_SkyBison_01.txt
+.\scripts\run_match.ps1 .\algos\Bumi .\algos\SkyBison     > Bumi_SkyBison_02.txt
+.\scripts\run_match.ps1 .\algos\Bumi .\algos\Moon         > Bumi_Moon_00.txt
+.\scripts\run_match.ps1 .\algos\Bumi .\algos\Moon         > Bumi_Moon_01.txt
+.\scripts\run_match.ps1 .\algos\Bumi .\algos\Moon         > Bumi_Moon_02.txt
+.\scripts\run_match.ps1 .\algos\Bumi .\algos\Boulder      > Bumi_Boulder_00.txt
+.\scripts\run_match.ps1 .\algos\Bumi .\algos\Boulder      > Bumi_Boulder_01.txt
+.\scripts\run_match.ps1 .\algos\Bumi .\algos\Boulder      > Bumi_Boulder_02.txt
+.\scripts\run_match.ps1 .\algos\Bumi .\algos\JongJong     > Bumi_JongJong_00.txt
+.\scripts\run_match.ps1 .\algos\Bumi .\algos\JongJong     > Bumi_JongJong_01.txt
+.\scripts\run_match.ps1 .\algos\Bumi .\algos\JongJong     > Bumi_JongJong_02.txt
+.\scripts\run_match.ps1 .\algos\Bumi .\algos\Gyatso       > Bumi_Gyatso_00.txt
+.\scripts\run_match.ps1 .\algos\Bumi .\algos\Gyatso       > Bumi_Gyatso_01.txt
+.\scripts\run_match.ps1 .\algos\Bumi .\algos\Gyatso       > Bumi_Gyatso_02.txt
+.\scripts\run_match.ps1 .\algos\Bumi .\algos\Paku         > Bumi_Paku_00.txt
+.\scripts\run_match.ps1 .\algos\Bumi .\algos\Paku         > Bumi_Paku_01.txt
+.\scripts\run_match.ps1 .\algos\Bumi .\algos\Paku         > Bumi_Paku_02.txt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
